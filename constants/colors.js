0ecefbefb15a2c4f8f4a84ffd4852b8fe7b4cb41
Thomas Labrusse <labrusse.thomas@gmail.com>
2022-09-15 15:58:11 +0200

    new check and warning colors

--- constants/colors.js
+++ constants/colors.js
@@ -5,6 +5,7 @@ export const Colors = {
 	grey100: '#eeeeee',
 	grey300: '#8f8f8f',
 	grey400: '#7f7f7f',
-	warning: '#a93b3b',
-	check: '#13A435',
+	warning: '#c55959',
+	// warning: '#a93b3b',
+	check: '#84A98C',
 }
