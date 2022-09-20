19aa8375c4af3365cfabd4fbe773b088fd42926d
Thomas Labrusse <labrusse.thomas@gmail.com>
2022-09-20 07:30:40 +0200

    improved codebase readability

--- data/database.js
+++ data/database.js
@@ -4,6 +4,65 @@ import * as SQLite from 'expo-sqlite'
 
 const db = SQLite.openDatabase('db.db')
 
+// ##################### NOTE: TABLES SETUP ###############################
+
+const setupDatabaseAsync = async () => {
+	return new Promise((resolve, reject) => {
+		db.transaction(
+			(tx) => {
+				tx.executeSql(
+					`CREATE TABLE IF NOT EXISTS habits (habitid INTEGER PRIMARY KEY NOT NULL, description TEXT NOT NULL, category TEXT CHECK(category IN ('productivity', 'health', 'family/friends')) NOT NULL, why TEXT NOT NULL, frequency TEXT CHECK( frequency IN ('day', 'week')) NOT NULL, reps INTEGER NOT NULL)`
+				)
+				tx.executeSql(
+					`CREATE TABLE IF NOT EXISTS validations (validationid INTEGER PRIMARY KEY NOT NULL, validationdate TEXT, validationcheck TEXT, habitid INTEGER NOT NULL, FOREIGN KEY(habitid) REFERENCES habits(habitid) ON DELETE CASCADE)`
+				)
+			},
+			(_, error) => {
+				console.log('db error creating tables')
+				console.log(error)
+				reject(error)
+			},
+			(_, success) => {
+				console.log('DB successfully setted up with 2 tables')
+				resolve(success)
+			}
+		)
+	})
+}
+
+// ##################### NOTE: HABITS SETUP ###############################
+
+const addHabitAsync = async (habit, successFunc) => {
+	return new Promise((resolve, reject) => {
+		console.log('Trying to add new habit:', habit)
+		db.transaction(
+			(tx) => {
+				tx.executeSql(
+					`INSERT INTO habits(description, category, why, frequency, reps) VALUES(?,?,?,?,?)`,
+					[
+						habit.description,
+						habit.category,
+						habit.why,
+						habit.frequency,
+						habit.reps,
+					]
+				)
+			},
+			(_, error) => {
+				console.log('DB error on creating new habit')
+				console.log(error)
+				reject(error)
+			},
+			(_, success) => {
+				// Success function = refresh context state base on current DB
+				successFunc()
+				console.log('New habit created successfully')
+				resolve(success)
+			}
+		)
+	})
+}
+
 const getHabits = (successFunc) => {
 	db.transaction(
 		(tx) => {
@@ -21,6 +80,46 @@ const getHabits = (successFunc) => {
 		}
 	)
 }
+
+// TODO: update habit in DB function
+
+const deleteHabitAsync = async (id, successFunc) => {
+	return new Promise((resolve, reject) => {
+		console.log('Trying to delete new habit:', id)
+		db.transaction(
+			(tx) => {
+				tx.executeSql(
+					`DELETE FROM habits WHERE habitid = ?`,
+					[id],
+					(_, result) => {
+						console.log('Habit element successfully deleted')
+						resolve(result)
+					},
+					(_, error) => {
+						console.log('error deleting habit element')
+						console.log('Error log:', error)
+						reject(error)
+					}
+				)
+			},
+			(_, error) => {
+				console.log('DB error on deleting habit with id:', id)
+				console.log(error)
+				reject(error)
+			},
+			(_, success) => {
+				// Success function = refresh context state base on current DB
+				successFunc()
+				console.log('Habit with id :' + id + 'deleted')
+				resolve(success)
+			}
+		)
+	})
+}
+
+// ##################### NOTE: VALIDATIONS SETUP ###############################
+
+// TODO: create validation
 
 const getValidations = (habitid, successFunc) => {
 	console.log('Getting validation for ID:', habitid)
@@ -46,31 +145,9 @@ const getValidations = (habitid, successFunc) => {
 	)
 }
 
-// TODO: update habit in DB function
+// TODO: delete validation (only when deleting habit ?)
 
-const setupDatabaseAsync = async () => {
-	return new Promise((resolve, reject) => {
-		db.transaction(
-			(tx) => {
-				tx.executeSql(
-					`CREATE TABLE IF NOT EXISTS habits (habitid INTEGER PRIMARY KEY NOT NULL, description TEXT NOT NULL, category TEXT CHECK(category IN ('productivity', 'health', 'family/friends')) NOT NULL, why TEXT NOT NULL, frequency TEXT CHECK( frequency IN ('day', 'week')) NOT NULL, reps INTEGER NOT NULL)`
-				)
-				tx.executeSql(
-					`CREATE TABLE IF NOT EXISTS validations (validationid INTEGER PRIMARY KEY NOT NULL, validationdate TEXT, validationcheck TEXT, habitid INTEGER NOT NULL, FOREIGN KEY(habitid) REFERENCES habits(habitid) ON DELETE CASCADE)`
-				)
-			},
-			(_, error) => {
-				console.log('db error creating tables')
-				console.log(error)
-				reject(error)
-			},
-			(_, success) => {
-				console.log('DB successfully setted up with 2 tables')
-				resolve(success)
-			}
-		)
-	})
-}
+// ################# NOTE: DEVELOPMENT ONLY ########################
 
 const dropDatabaseTablesAsync = async () => {
 	return new Promise((resolve, reject) => {
@@ -153,70 +230,7 @@ const setupFirstValidationAsync = async () => {
 	})
 }
 
-const addHabitAsync = async (habit, successFunc) => {
-	return new Promise((resolve, reject) => {
-		console.log('Trying to add new habit:', habit)
-		db.transaction(
-			(tx) => {
-				tx.executeSql(
-					`INSERT INTO habits(description, category, why, frequency, reps) VALUES(?,?,?,?,?)`,
-					[
-						habit.description,
-						habit.category,
-						habit.why,
-						habit.frequency,
-						habit.reps,
-					]
-				)
-			},
-			(_, error) => {
-				console.log('DB error on creating new habit')
-				console.log(error)
-				reject(error)
-			},
-			(_, success) => {
-				// Success function = refresh context state base on current DB
-				successFunc()
-				console.log('New habit created successfully')
-				resolve(success)
-			}
-		)
-	})
-}
-
-const deleteHabitAsync = async (id, successFunc) => {
-	return new Promise((resolve, reject) => {
-		console.log('Trying to delete new habit:', id)
-		db.transaction(
-			(tx) => {
-				tx.executeSql(
-					`DELETE FROM habits WHERE habitid = ?`,
-					[id],
-					(_, result) => {
-						console.log('Habit element successfully deleted')
-						resolve(result)
-					},
-					(_, error) => {
-						console.log('error deleting habit element')
-						console.log('Error log:', error)
-						reject(error)
-					}
-				)
-			},
-			(_, error) => {
-				console.log('DB error on deleting habit with id:', id)
-				console.log(error)
-				reject(error)
-			},
-			(_, success) => {
-				// Success function = refresh context state base on current DB
-				successFunc()
-				console.log('Habit with id :' + id + 'deleted')
-				resolve(success)
-			}
-		)
-	})
-}
+// #######################################################################
 
 export const database = {
 	getHabits,
